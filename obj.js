d15be60099a14cf61bd5ac763dcd679755d8a5f1
dinachispita91 <dinafh91@gmail.com>
2024-07-23 20:24:45 +0200

    cc

---
+++ obj.js
@@ -0,0 +1,16 @@
+
+// let articulos ={
+//   nombre: 'tacon negro',
+//   precio:'49,30',
+//   imagen:'/images.jpg'
+// } 
+// {
+//   nombre: 'sandalia azul',
+//   precio:'49.99',
+//   imagen:'./'
+// } 
+// {
+//   nombre: 'tacon negro',
+//   precio:'49,30',
+//   imagen:'/images.jpg'
+// } 
